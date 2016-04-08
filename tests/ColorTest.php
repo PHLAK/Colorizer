c0e8416d38b214d29a6c334f98bcfa46cd256596
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2016-04-07 23:22:03 -0700

    Lots of changes to tests to make them a bit more readable

--- tests/ColorTest.php
+++ tests/ColorTest.php
@@ -1,43 +1,41 @@
 <?php
 
-    use Colorizer\Color;
-
     class ColorTest extends PHPUnit_Framework_TestCase {
 
         protected $color;
 
+        /** @test */
         public function setUp() {
-            $this->color = new Color(12, 162, 234);
+            $this->color = new Colorizer\Color(12, 162, 234);
         }
 
-        public function testColorObject() {
-            $this->assertInstanceOf('Colorizer\Color', $this->color);
-        }
-
-        public function testRedValue() {
+        /** @test */
+        public function it_has_a_red_value() {
             $this->assertEquals(12, $this->color->red());
             $this->assertEquals(12, $this->color->r());
         }
 
-        public function testGreenValue() {
+        /** @test */
+        public function it_has_a_green_value() {
             $this->assertEquals(162, $this->color->green());
             $this->assertEquals(162, $this->color->g());
         }
 
-        public function testBlueValue() {
+        /** @test */
+        public function it_has_a_blue_value() {
             $this->assertEquals(234, $this->color->blue());
             $this->assertEquals(234, $this->color->b());
         }
 
-        public function testRGBValue() {
+        /** @test */
+        public function test_it_has_an_rgb_value() {
             $this->assertEquals('rgb(12, 162, 234)', $this->color->rgb());
         }
 
-        public function testNormalize() {
-            $this->assertEquals(
-                new Color(64, 162, 224),
-                $this->color->normalize(64, 224)
-            );
+        /** @test */
+        public function test_it_can_be_normalized() {
+            $normalize = $this->color->normalize(64, 224);
+            $this->assertEquals(new Colorizer\Color(64, 162, 224), $normalize);
         }
 
     }
--- tests/ColorizeTest.php
+++ tests/ColorizeTest.php
@@ -1,43 +1,33 @@
 <?php
 
-    use Colorizer\Colorize;
-
     class ColorizeTest extends PHPUnit_Framework_TestCase {
 
-        public function testColorizeToRGB() {
+        protected $colorize;
 
-            // Initialize Colorizer
-            $colorize = new Colorize();
-
-            // Test the output
-            $this->assertEquals(
-                'rgb(165, 196, 254)',
-                $rgbColor = $colorize->toRGB('foo')
-            );
-
+        public function setup() {
+            $this->colorize = new Colorizer\Colorize();
         }
 
-        public function testColorizeToHex() {
-
-            // Initialize Colorizer
-            $colorize = new Colorize();
-
-            // Test the output
-            $this->assertEquals('#a5c4fe', $colorize->toHex('foo'));
-
+        /** @test */
+        public function it_can_be_converted_to_rgb() {
+            $this->assertEquals('rgb(165, 196, 254)', $this->colorize->toRGB('foo'));
         }
 
-        public function testNormalize() {
+        /** @test */
+        public function it_can_be_converted_to_hex() {
+            $this->assertEquals('#a5c4fe', $this->colorize->toHex('foo'));
+        }
 
-            // Initialize Colorizer
-            $colorize = new Colorize(64, 224);
+        /** @test */
+        public function it_can_be_normalized_via_the_constructor() {
+            $normalized = new Colorizer\Colorize(64, 224);
+            $this->assertEquals('rgb(64, 64, 224)', $normalized->toRGB('Taco bueno!'));
+        }
 
-            // Test the output
-            $this->assertEquals(
-                'rgb(64, 64, 224)',
-                $colorize->toRGB('Taco bueno!')
-            );
-
+        /** @test */
+        public function it_can_be_normalized_via_a_method() {
+            $normalized = $this->colorize->normalize(64, 224);
+            $this->assertEquals('rgb(64, 64, 224)', $normalized->toRGB('Taco bueno!'));
         }
 
     }
